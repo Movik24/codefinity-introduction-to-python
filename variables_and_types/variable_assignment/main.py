1d3412987b725bdaa90439d24db181452b9cb07b
Movik24 <>
2026-02-17 10:31:36 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,8 @@
+item_name = "Eggs"
+print ("Item:" , item_name)
+item_price = 3.99
+print ("Per egg:" , item_price)
+item_stock_quantity = 120
+print ("Number of eggs in stock:" , item_stock_quantity)
+in_stock = True
+print ("Eggs are in stock:" , in_stock)
