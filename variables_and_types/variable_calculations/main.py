48b36a613a91dc74beeecf6d1c9fb97c7371bc03
Movik24 <>
2026-02-18 03:36:14 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,8 @@
+item_name = "Soda"
+print (f"Item:{item_name}")
+item_price = 6.99 
+print (f"Price per case:${item_price}")
+purchase_quantity = 5 
+total_cost = item_price * purchase_quantity
+print (f"Number of cases:{purchase_quantity}")
+print (f"Total cost:${total_cost}")
