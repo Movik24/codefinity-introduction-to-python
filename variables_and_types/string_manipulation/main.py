418c823d87aa8f6706cff343c7f0a0c49b647163
Movik24 <>
2026-02-28 13:07:09 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+user_name = "Water"
+print (user_name[-4])
+print (len(user_name))
+user_k = 500 
+user_m = 300 
+total_cost = user_k + user_m
+print (f"Total cost:{total_cost}")
+user_j = "Strawberries"
+user_f = user_j[0:5]
+print("Sliced part:" , user_f)
